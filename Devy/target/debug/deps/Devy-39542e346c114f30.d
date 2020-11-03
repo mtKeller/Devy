059274b58e3365af6760fd58cb2e44849d1bbd0c
mtKeller <micah.t.keller@gmail.com>
2020-11-02 22:52:39 -0800

    Naming conv

---
+++ Devy/target/debug/deps/Devy-39542e346c114f30.d
@@ -0,0 +1,5 @@
+c:\Users\micah\projects\Devy\Devy\target\debug\deps\Devy-39542e346c114f30.rmeta: src\main.rs
+
+c:\Users\micah\projects\Devy\Devy\target\debug\deps\Devy-39542e346c114f30.d: src\main.rs
+
+src\main.rs:
